0604560e1eff398c2ba62250ad85c67e58253918
AMJL-16 <ozaurelio@gmail.com>
2023-03-01 16:19:38 +0000

    add comments, iteration and  eventlisteners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+// wait for the DOM to finish loading before running the game 
+// get the button elements and add event listeners to them
+
+// 1st eventlistener: code to be executed when the page has finished loading 
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByName("button");
+
+// So  we have five buttons in our  HTML file. four game buttons, one submit button 
+// we are using the getElementsbyTagName method to return all of those  buttons.
+    
+    //  iterate through an array, more modern then using indexing we use let..of..
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+
+        }))
+
+    }
+})
+
+// 2nd eventlistener; code to be executed when the user clicks a button
 //to run game
 function runGame() {
 
