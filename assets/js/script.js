14689638e332202e6855138390d78eb9890d1941
AMJL-16 <ozaurelio@gmail.com>
2023-03-01 15:48:59 +0000

    set up structure of the game

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+//to run game
+function runGame() {
+
+}
+// to check answer
+function checkAnswer() {
+
+}
+
+// to calculate answer
+function calculateCorrectAnswer() {
+
+}
+
+// to increment score
+function incrementScore() {
+
+}
+
+// to increment wrong anwer
+function incrementWrongAnswer() {
+
+}
+
+// addition question
+function displayAdditionQuestion() {
+
+}
+// subtract question
+function displaySubtractQuestion() {
+
+}
+// multiply question
+function displayMultiplyQuestion() {
+
+}
+// division question
+function displayDivideQuestion() {
+
+}
